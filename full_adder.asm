f29551f621d02feabeddf77a647b685fc940dc43
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-12-03 21:55:38 +0530

    Completed code

--- full_adder.asm
+++ full_adder.asm
@@ -3,14 +3,39 @@ main:
 ldi r16,0b11000000		;for port d configuration for LED output
 out ddrd,r16
 ldi r16,0b00000000		;for port b configuration for switches
-
+out ddrb,r16
+loop:
 in r16,pinb
 andi r16,0b00000111
 
-ldi r17,0x00			;a
-ldi r18,0x00			;b
-ldi r19,0x00			;c
+ldi r23 ,0x00			;real sum
+
+;input from a
+mov r22,r16
+andi r22,0x01
+add r23,r22
 
 
-ldi r20,0x00			;sum
-ldi r21,0x00			;carry
+;input from b
+mov r22,r16
+andi r22,2
+lsr r22
+add r23 , r22
+
+;input from c
+mov r22,r16
+andi r22,4
+lsr r22
+lsr r22
+add r23 , r22
+
+
+;write output
+lsl r23
+lsl r23
+lsl r23
+lsl r23
+lsl r23
+lsl r23
+out portd,r23
+rjmp loop
